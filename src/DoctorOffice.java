b562235f4fcf6b87e5be6d5325010b76792407de
aminahaid <aminahaid@gmail.com>
2020-04-16 08:11:35 -0500

    t commit

---
+++ src/DoctorOffice.java
@@ -0,0 +1,20 @@
+import java.util.Date;
+
+public class DoctorOffice {
+    public static void main (String[] args)
+    {
+        String patientName = "Stacey Mason";
+boolean hasInsurance = false;
+double temp = 102.2;
+double numVisits = 5.2;
+int PriceCoPay = 10;
+
+Date date= new Date(2020/12/31);
+String number = "240-0000-000";
+System.out.println("Patient detail: "+"\n"+"Name: "+ patientName + "\n"+"Patient has insurance: "+ false +"\n"
+        +"Temperature: " + temp + "\n" + "Number of Visits: "+ 5.2+ "\n"+"copay: " + PriceCoPay
++"\n"+ "Next Appointment date: "+ date +"\n" +"Contact number: "+ number );
+
+
+    }
+}
